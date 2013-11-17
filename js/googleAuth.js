cded08930f3cc78745e893491c82cb87eb0e4016
Md Khan <khan4019@gmail.com>
2013-11-16 22:44:41 -0600

    oAuth intial

---
+++ js/googleAuth.js
@@ -0,0 +1,89 @@
+//source: http://www.gethugames.in/blog/2012/04/authentication-and-authorization-for-google-apis-in-javascript-popup-window-tutorial.html
+
+var OAUTHURL    =   'https://accounts.google.com/o/oauth2/auth?';
+var VALIDURL    =   'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
+var SCOPE       =   'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
+var CLIENTID    =   '638348368613.apps.googleusercontent.com';
+var REDIRECT    =   'http://www.thatjsdude.com/'
+var LOGOUT      =   'http://accounts.google.com/Logout';
+var TYPE        =   'token';
+var _url        =   OAUTHURL + 'scope=' + SCOPE + '&client_id=' + CLIENTID + '&redirect_uri=' + REDIRECT + '&response_type=' + TYPE;
+var acToken;
+var tokenType;
+var expiresIn;
+var user;
+var loggedIn    =   false;
+
+function login() {
+    var win =  window.open(_url, "windowname1", 'width=800, height=600');             
+    var pollTimer   =   window.setInterval(function() { 
+        
+        try {
+            var url = win.document.URL;                     
+            if (url.indexOf(REDIRECT) != -1) {
+                window.clearInterval(pollTimer);
+                acToken =   gup(url, 'access_token');
+                tokenType = gup(url, 'token_type');
+                expiresIn = gup(url, 'expires_in');
+                win.close();
+
+                validateToken(acToken);
+            }
+        } catch(e) {
+            console.log('error '+e);
+        }
+    }, 500);
+}
+
+function validateToken(token) {
+    $.ajax({
+        url: VALIDURL + token,
+        data: null,
+        success: function(responseText){  
+            getUserInfo();
+            loggedIn = true;
+            $('#loginText').hide();
+            $('#logoutText').show();
+        },
+        error:function(){
+            console.log('Failed to validateToken');
+        },
+        dataType: "jsonp"  
+    });
+}
+
+function getUserInfo() {
+    $.ajax({
+        url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
+        data: null,
+        success: function(resp) {
+            user    =   resp;            
+            $('#uName').text('Welcome ' + user.name);
+            //$('#imgHolder').attr('src', user.picture);
+        },
+        error:function(){
+            console.log('Failed to get userinfo');
+        },
+        dataType: "jsonp"
+    });
+}
+
+//credits: http://www.netlobo.com/url_query_string_javascript.html
+function gup(url, name) {
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\#&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( url );
+    if( results == null )
+        return "";
+    else
+        return results[1];
+}
+
+function startLogoutPolling() {
+    $('#loginText').show();
+    $('#logoutText').hide();
+    loggedIn = false;
+    $('#uName').text('Welcome ');
+    //$('#imgHolder').attr('src', 'none.jpg');
+}
