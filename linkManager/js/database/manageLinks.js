f8f5d196bc6e84ee3202d1d16b31796f7b9c59c3
Md Khan <khan4019@gmail.com>
2013-12-29 19:55:05 -0600

    update paths

--- linkManager/js/database/manageLinks.js
+++ linkManager/js/database/manageLinks.js
@@ -1,14 +1,14 @@
-function createLink () {
+function createLink (data) {
 	$.ajax({
 	  type: "POST",
-	  url: '../../dbHandlers/createTeam.php',
+	  url: '../dbHandlers/createLink.php',
 	  data: data,
 	  dataType: 'json',
 	  success: function(data){
 		console.log('success');
 	  },
 	  error:function(x,e){
-		console.log(e);
+		console.log(x.responseText);
 	  }
 	});
 	return false;
